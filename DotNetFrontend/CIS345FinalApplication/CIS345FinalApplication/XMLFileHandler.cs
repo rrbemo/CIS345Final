3ec632804a9088912f9b8d02bcbd5caba8946744
Kevin Barthman <kbart897@uwsp.edu>
2014-11-23 01:45:49 -0600

    Implemented Lucene/XML functionality in C#
    Make sure in the C# project to add a reference to all of the DLL's in
    the 'lib' folder (some might not be necessary, but I didn't feel like
    testing all of them)

---
+++ DotNetFrontend/CIS345FinalApplication/CIS345FinalApplication/XMLFileHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.xml.sax;
+using org.xml.sax.helpers;
+
+namespace CIS345FinalApplication
+{
+    public class XMLFileHandler : DefaultHandler
+    {
+        //private int level = 0;
+        //public string output = "";
+        private LuceneIndexHandler indexHandler;
+        private string currentElement;
+        private List<string> elements;
+
+        public XMLFileHandler(LuceneIndexHandler indexer)
+        {
+            //TODO: constructor
+            indexHandler = indexer;
+            elements = new List<string>();
+        }
+
+        public List<string> GetElements()
+        {
+            return elements;
+        }
+
+        public override void startElement (String uri, String localName, String qName, Attributes atts )
+        {
+            currentElement = localName;
+            if (!elements.Contains(localName))
+            {
+                elements.Add(localName);
+            }
+            //level += 1;
+            //string spacing = "";
+            //for (int i = 0; i < level; i++)
+            //{
+            //    spacing += " ";
+            //}
+            //output += spacing + localName + ":" + level + ": ";
+            //nodeId += 1;
+            //System.out.println(localName+":"+level+":"+nodeId);
+            //output += localName+":"+level+":"+nodeId;
+	    }
+
+        public override void characters(char[] ch, int start, int length)
+        {
+            if (String.IsNullOrEmpty(currentElement))
+            {
+                return;
+            }
+            string currentElementContent = "";
+            for (int i = start; i < start + length; i++)
+            {
+                currentElementContent += ch[i];
+            }
+
+            indexHandler.AddDocument(currentElement, currentElementContent);
+        }
+
+        public override void endElement(String uri, String localName, String qName)
+        {
+            currentElement = "";
+            //...not sure what to do???
+            //level -= 1;
+            //output += Environment.NewLine;
+        }
+    }
+}
